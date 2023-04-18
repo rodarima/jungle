a5449067a76a5923dd9c12201ed73531070ba9ff
Rodrigo Arias <rodrigo.arias@bsc.es>
2023-04-18 18:56:31 +0200

    Add configuration for xeon01

---
+++ xeon01/configuration.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../common/main.nix
+  ];
+
+  # Select the this using the ID to avoid mismatches
+  boot.loader.grub.device = "/dev/disk/by-id/wwn-0x55cd2e414d53566c";
+
+  networking = {
+    hostName = "xeon01";
+    interfaces.eno1.ipv4.addresses = [ {
+      address = "10.0.40.1";
+      prefixLength = 24;
+    } ];
+    interfaces.ibp5s0.ipv4.addresses = [ {
+      address = "10.0.42.1";
+      prefixLength = 24;
+    } ];
+  };
+}
