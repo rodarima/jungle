69bb2128dbb3fb624871278d3989767de885cd67
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-04-26 17:36:36 +0200

    Lock flakes and add inputs

--- common/main.nix
+++ common/main.nix
@@ -6,7 +6,6 @@
     ./fs.nix
     ./hw.nix
     ./net.nix
-    ./overlays.nix
     ./ssh.nix
     ./users.nix
   ];
@@ -38,11 +37,12 @@
 
   programs.zsh.enable = true;
   programs.zsh.histSize = 100000;
+  users.defaultUserShell = pkgs.zsh;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
+  #system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
