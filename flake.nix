87c4521de34638f1d3b594559bc9137fb6f2899c
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-04-28 11:18:37 +0200

    Update nixpkgs to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
     bscpkgs.url = "git+https://pm.bsc.es/gitlab/rarias/bscpkgs.git";
