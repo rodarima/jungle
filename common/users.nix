0b57bbc6e3af95dcc5dd4849dcea33f79bb317bd
Aleix Roca Nonell <aleix.rocanonell@bsc.es>
2023-06-14 16:16:46 +0200

    Move arocanon user from xeon08 to common

--- common/users.nix
+++ common/users.nix
@@ -3,14 +3,26 @@
 {
   users = {
     mutableUsers = false;
-    users.rarias = {
-      uid = 1880;
-      isNormalUser = true;
-      home = "/home/Computational/rarias";
-      description = "Rodrigo Arias";
-      group = "Computational";
-      extraGroups = [ "wheel" ];
-      hashedPassword = "$6$u06tkCy13enReBsb$xiI.twRvvTfH4jdS3s68NZ7U9PSbGKs5.LXU/UgoawSwNWhZo2hRAjNL5qG0/lAckzcho2LjD0r3NfVPvthY6/";
+    users = {
+      rarias = {
+        uid = 1880;
+        isNormalUser = true;
+        home = "/home/Computational/rarias";
+        description = "Rodrigo Arias";
+        group = "Computational";
+        extraGroups = [ "wheel" ];
+        hashedPassword = "$6$u06tkCy13enReBsb$xiI.twRvvTfH4jdS3s68NZ7U9PSbGKs5.LXU/UgoawSwNWhZo2hRAjNL5qG0/lAckzcho2LjD0r3NfVPvthY6/";
+      };
+
+      arocanon = {
+        uid = 1042;
+        isNormalUser = true;
+        home = "/home/Computational/arocanon";
+        description = "Aleix Roca";
+        group = "Computational";
+        extraGroups = [ "wheel" ];
+        hashedPassword = "$6$hliZiW4tULC/tH7p$pqZarwJkNZ7vS0G5llWQKx08UFG9DxDYgad7jplMD8WkZh5k58i4dfPoWtnEShfjTO6JHiIin05ny5lmSXzGM/";
+      };
     };
 
     groups = {
--- xeon08/users.nix
+++ xeon08/users.nix
@@ -1,18 +1,6 @@
 { ... }:
 
 {
-  users = {
-    users.arocanon = {
-      uid = 1042;
-      isNormalUser = true;
-      home = "/home/Computational/arocanon";
-      description = "Aleix Roca";
-      group = "Computational";
-      extraGroups = [ "wheel" ];
-      hashedPassword = "$6$hliZiW4tULC/tH7p$pqZarwJkNZ7vS0G5llWQKx08UFG9DxDYgad7jplMD8WkZh5k58i4dfPoWtnEShfjTO6JHiIin05ny5lmSXzGM/";
-    };
-  };
-
   security.sudo.extraRules= [{
     users = [ "arocanon" ];
     commands = [{
