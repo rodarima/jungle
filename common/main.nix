76c10ec22ee178d61dfbcb88009c332c63f8bae3
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-06-14 12:05:15 +0200

    Add ncdu to system packages

--- common/main.nix
+++ common/main.nix
@@ -14,6 +14,7 @@
   environment.systemPackages = with pkgs; [
     vim wget git htop tmux pciutils tcpdump ripgrep nix-index nixos-option
     nix-diff ipmitool freeipmi ethtool lm_sensors ix cmake gnumake file tree
+    ncdu
   ];
 
   systemd.services."serial-getty@ttyS0" = {
