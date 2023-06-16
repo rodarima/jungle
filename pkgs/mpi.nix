5b41670f3666d9e8432d5253ea2999643d7a0e6d
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-06-16 15:54:55 +0200

    Replace mpi inside bsc attribute

--- pkgs/mpi.nix
+++ pkgs/mpi.nix
@@ -1,25 +1,28 @@
 let
   overlay = final: prev:
   {
-    mpich = with final; prev.mpich.overrideAttrs (old: {
-      buildInput = old.buildInputs ++ [
-        libfabric
-        pmix
-      ];
-      configureFlags = [
-        "--enable-shared"
-        "--enable-sharedlib"
-        "--with-pm=no"
-        "--with-device=ch4:ofi"
-        "--with-pmi=pmix"
-        "--with-pmix=${final.pmix}"
-        "--with-libfabric=${final.libfabric}"
-        "--enable-g=log"
-      ] ++ lib.optionals (lib.versionAtLeast gfortran.version "10") [
-        "FFLAGS=-fallow-argument-mismatch" # https://github.com/pmodels/mpich/issues/4300
-        "FCFLAGS=-fallow-argument-mismatch"
-      ];
-    });
+    bsc = prev.bsc.extend (bsc: {
+      mpi2 = bsc.mpich;
+      mpich = with final; prev.mpich.overrideAttrs (old: {
+        buildInput = old.buildInputs ++ [
+          libfabric
+          pmix
+        ];
+        configureFlags = [
+          "--enable-shared"
+          "--enable-sharedlib"
+          "--with-pm=no"
+          "--with-device=ch4:ofi"
+          "--with-pmi=pmix"
+          "--with-pmix=${final.pmix}"
+          "--with-libfabric=${final.libfabric}"
+          "--enable-g=log"
+        ] ++ lib.optionals (lib.versionAtLeast gfortran.version "10") [
+          "FFLAGS=-fallow-argument-mismatch" # https://github.com/pmodels/mpich/issues/4300
+          "FCFLAGS=-fallow-argument-mismatch"
+        ];
+      });
+    };
   };
 in
 
