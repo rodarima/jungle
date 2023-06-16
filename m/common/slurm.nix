dfea0be2d9378aeca35f1692599139789e4ad8f2
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-06-16 12:00:54 +0200

    Set the name of the slurm cluster to jungle

--- m/common/slurm.nix
+++ m/common/slurm.nix
@@ -4,7 +4,7 @@
   services.slurm = {
     client.enable = true;
     controlMachine = "hut";
-    clusterName = "owl";
+    clusterName = "jungle";
     nodeName = [
       "owl[1,2]  Sockets=2 CoresPerSocket=14 ThreadsPerCore=2 Feature=owl"
       "hut       Sockets=2 CoresPerSocket=14 ThreadsPerCore=2"
