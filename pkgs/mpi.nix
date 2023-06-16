f2434a17c2ed742cf8774b158d1b585d6c812645
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-06-16 16:02:25 +0200

    Use explicit order in overlays

--- pkgs/mpi.nix
+++ pkgs/mpi.nix
@@ -1,31 +1,25 @@
-let
-  overlay = final: prev:
-  {
-    bsc = prev.bsc.extend (bsc: {
-      mpi2 = bsc.mpich;
-      mpich = with final; prev.mpich.overrideAttrs (old: {
-        buildInput = old.buildInputs ++ [
-          libfabric
-          pmix
-        ];
-        configureFlags = [
-          "--enable-shared"
-          "--enable-sharedlib"
-          "--with-pm=no"
-          "--with-device=ch4:ofi"
-          "--with-pmi=pmix"
-          "--with-pmix=${final.pmix}"
-          "--with-libfabric=${final.libfabric}"
-          "--enable-g=log"
-        ] ++ lib.optionals (lib.versionAtLeast gfortran.version "10") [
-          "FFLAGS=-fallow-argument-mismatch" # https://github.com/pmodels/mpich/issues/4300
-          "FCFLAGS=-fallow-argument-mismatch"
-        ];
-      });
-    };
-  };
-in
-
+final: prev:
 {
-  nixpkgs.overlays = [ overlay ];
+  bsc = prev.bsc.extend (bsc: {
+    mpi2 = bsc.mpich;
+    mpich = with final; prev.mpich.overrideAttrs (old: {
+      buildInput = old.buildInputs ++ [
+        libfabric
+        pmix
+      ];
+      configureFlags = [
+        "--enable-shared"
+        "--enable-sharedlib"
+        "--with-pm=no"
+        "--with-device=ch4:ofi"
+        "--with-pmi=pmix"
+        "--with-pmix=${final.pmix}"
+        "--with-libfabric=${final.libfabric}"
+        "--enable-g=log"
+      ] ++ lib.optionals (lib.versionAtLeast gfortran.version "10") [
+        "FFLAGS=-fallow-argument-mismatch" # https://github.com/pmodels/mpich/issues/4300
+        "FCFLAGS=-fallow-argument-mismatch"
+      ];
+    });
+  });
 }
