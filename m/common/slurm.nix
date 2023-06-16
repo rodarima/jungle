530958496beeeb393d3fab4f1e09508988035c5f
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-06-16 14:16:14 +0200

    Add coments in slurm config

--- m/common/slurm.nix
+++ m/common/slurm.nix
@@ -10,7 +10,15 @@
       "hut       Sockets=2 CoresPerSocket=14 ThreadsPerCore=2"
     ];
     extraConfig = ''
+      # Use PMIx for MPI by default. It works okay with MPICH and OpenMPI, but
+      # not with Intel MPI. For that use the compatibility shim libpmi.so
+      # setting I_MPI_PMI_LIBRARY=$pmix/lib/libpmi.so while maintaining the PMIx
+      # library in SLURM (--mpi=pmix). See more details here:
+      # https://pm.bsc.es/gitlab/rarias/jungle/-/issues/16
       MpiDefault=pmix
+
+      # When a node reboots return that node to the slurm queue as soon as it
+      # becomes operative again.
       ReturnToService=2
     '';
   };
