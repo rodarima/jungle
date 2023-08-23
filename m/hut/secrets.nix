d81d9d58e1e03c9c0eaa17da94a13eaaef03c8c4
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-08-23 17:15:26 +0200

    Add rarias key for secrets

--- m/hut/secrets.nix
+++ m/hut/secrets.nix
@@ -1,9 +1,10 @@
 let
-  root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIII/1TNArcwA6D47mgW4TArwlxQRpwmIGiZDysah40Gb";
-  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICO7jIp6JRnRWTMDsTB/aiaICJCl4x8qmKMPSs4lCqP1";
-  systems = [ root system ];
+  rarias  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE1oZTPtlEXdGt0Ak+upeCIiBdaDQtcmuWoTUCVuSVIR rarias@hut";
+  root    = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIII/1TNArcwA6D47mgW4TArwlxQRpwmIGiZDysah40Gb";
+  hut     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICO7jIp6JRnRWTMDsTB/aiaICJCl4x8qmKMPSs4lCqP1";
+  default = [ rarias root hut ];
 in
 {
-  "secrets/ovni-token.age".publicKeys = systems;
-  "secrets/nosv-token.age".publicKeys = systems;
+  "secrets/ovni-token.age".publicKeys = default;
+  "secrets/nosv-token.age".publicKeys = default;
 }
