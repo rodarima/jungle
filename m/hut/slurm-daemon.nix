77cb3c494ec530c3cf8f33376d2c620863eef553
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-09-08 13:31:23 +0200

    Poweroff idle slurm nodes after 1 hour

--- m/hut/slurm-daemon.nix
+++ m/hut/slurm-daemon.nix
@@ -3,9 +3,5 @@
 {
   services.slurm = {
     server.enable = true;
-    partitionName = [
-      "owl Nodes=owl[1-2] Default=YES MaxTime=INFINITE State=UP"
-      "all Nodes=owl[1-2],hut Default=NO MaxTime=INFINITE State=UP"
-    ];
   };
 }
