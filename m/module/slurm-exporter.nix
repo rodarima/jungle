c0066c47447a853e719c55d9b4b9e2a213880702
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-09-22 10:13:06 +0200

    Remove user/group when using DynamicUsers

--- m/module/slurm-exporter.nix
+++ m/module/slurm-exporter.nix
@@ -6,17 +6,6 @@
 with lib;
 
 {
-  users = {
-    users."slurm-exporter" = {
-      description = "Prometheus slurm exporter service user";
-      isSystemUser = true;
-      group = "slurm-exporter";
-    };
-    groups = {
-      "slurm-exporter" = {};
-    };
-  };
-
   systemd.services."prometheus-slurm-exporter" = {
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
@@ -25,8 +14,6 @@ with lib;
       PrivateTmp = mkDefault true;
       WorkingDirectory = mkDefault "/tmp";
       DynamicUser = mkDefault true;
-      User = "slurm-exporter";
-      Group = "slurm-exporter";
       ExecStart = ''
         ${pkgs.prometheus-slurm-exporter}/bin/prometheus-slurm-exporter --listen-address "127.0.0.1:9341"
       '';
