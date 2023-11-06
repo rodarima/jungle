dd341902fcb17a4f26925201bebaa113efbf9fe8
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-11-06 23:03:56 +0100

    BSC packages are no longer in bsc attribute
    Reviewed-by: Aleix Roca Nonell <aleix.rocanonell@bsc.es>

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -1,29 +1,27 @@
 final: prev:
 {
-  bsc = prev.bsc.extend (bscFinal: bscPrev: {
-    # Set MPICH as default
-    mpi = bscFinal.mpich;
+  # Set MPICH as default
+  mpi = final.mpich;
 
-    # Configure the network for MPICH
-    mpich = with final; prev.mpich.overrideAttrs (old: {
-      buildInput = old.buildInputs ++ [
-        libfabric
-        pmix
-      ];
-      configureFlags = [
-        "--enable-shared"
-        "--enable-sharedlib"
-        "--with-pm=no"
-        "--with-device=ch4:ofi"
-        "--with-pmi=pmix"
-        "--with-pmix=${final.pmix}"
-        "--with-libfabric=${final.libfabric}"
-        "--enable-g=log"
-      ] ++ lib.optionals (lib.versionAtLeast gfortran.version "10") [
-        "FFLAGS=-fallow-argument-mismatch" # https://github.com/pmodels/mpich/issues/4300
-        "FCFLAGS=-fallow-argument-mismatch"
-      ];
-    });
+  # Configure the network for MPICH
+  mpich = with final; prev.mpich.overrideAttrs (old: {
+    buildInput = old.buildInputs ++ [
+      libfabric
+      pmix
+    ];
+    configureFlags = [
+      "--enable-shared"
+      "--enable-sharedlib"
+      "--with-pm=no"
+      "--with-device=ch4:ofi"
+      "--with-pmi=pmix"
+      "--with-pmix=${final.pmix}"
+      "--with-libfabric=${final.libfabric}"
+      "--enable-g=log"
+    ] ++ lib.optionals (lib.versionAtLeast gfortran.version "10") [
+      "FFLAGS=-fallow-argument-mismatch" # https://github.com/pmodels/mpich/issues/4300
+      "FCFLAGS=-fallow-argument-mismatch"
+    ];
   });
 
   # Update ceph to 18.2.0 until it lands in nixpkgs, see:
