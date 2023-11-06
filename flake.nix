268807d1d0338ac37d87b20c2b0cc8022240029a
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2023-11-06 17:50:38 +0100

    Switch bscpkgs URL to sourcehut
    Reviewed-by: Aleix Roca Nonell <aleix.rocanonell@bsc.es>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
-    bscpkgs.url = "git+https://pm.bsc.es/gitlab/rarias/bscpkgs.git";
+    bscpkgs.url = "git+https://git.sr.ht/~rodarima/bscpkgs";
     bscpkgs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
