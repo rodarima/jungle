834d3187e5a832d977827d3b15a0e468ddfe8b34
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2024-03-14 16:59:11 +0100

    Merge pmix outputs for MPICH
    MPICH expects headers and libraries to be present in the same directory.
    
    Reviewed-by: Aleix Roca Nonell <aleix.rocanonell@bsc.es>

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -4,10 +4,16 @@ final: prev:
   mpi = final.mpich;
 
   # Configure the network for MPICH
-  mpich = with final; prev.mpich.overrideAttrs (old: {
+  mpich = with final; let
+    # pmix comes with the libraries in .out and headers in .dev
+    pmixAll = symlinkJoin {
+      name = "pmix-all";
+      paths = [ pmix.dev pmix.out ];
+    };
+  in prev.mpich.overrideAttrs (old: {
     buildInput = old.buildInputs ++ [
       libfabric
-      pmix
+      pmixAll
     ];
     configureFlags = [
       "--enable-shared"
@@ -15,8 +21,8 @@ final: prev:
       "--with-pm=no"
       "--with-device=ch4:ofi"
       "--with-pmi=pmix"
-      "--with-pmix=${final.pmix}"
-      "--with-libfabric=${final.libfabric}"
+      "--with-pmix=${pmixAll}"
+      "--with-libfabric=${libfabric}"
       "--enable-g=log"
     ] ++ lib.optionals (lib.versionAtLeast gfortran.version "10") [
       "FFLAGS=-fallow-argument-mismatch" # https://github.com/pmodels/mpich/issues/4300
