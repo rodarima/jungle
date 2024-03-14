49be0f208c2591baca1d6a49049b39a56d29ad0f
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2024-03-14 16:44:21 +0100

    Remove nixseparatedebuginfod input
    It has been integrated in nixpkgs, so is no longer required.
    
    Reviewed-by: Aleix Roca Nonell <aleix.rocanonell@bsc.es>

--- m/module/debuginfod.nix
+++ m/module/debuginfod.nix
@@ -1,12 +1,3 @@
-{ theFlake, ... }:
-
-let
-  nixseparatedebuginfod = theFlake.inputs.nixseparatedebuginfod;
-in
 {
-  imports = [
-    nixseparatedebuginfod.nixosModules.default
-  ];
-
   services.nixseparatedebuginfod.enable = true;
 }
