d00f996f595748e3e520b1d72e222d21b84529ed
Aleix Boné <aleix.boneribo@bsc.es>
2025-06-03 17:59:17 +0200

    Use extra- for substituters and trusted-public-keys
    From the nix manual:
    
    > A configuration setting usually overrides any previous value. However,
    > for settings that take a list of items, you can prefix the name of the
    > setting by extra- to append to the previous value.
    
    Reviewed-by: Rodrigo Arias Mallo <rodrigo.arias@bsc.es>

--- m/module/hut-substituter.nix
+++ m/module/hut-substituter.nix
@@ -4,7 +4,7 @@
     # Don't add hut as a cache to itself
     assert config.networking.hostName != "hut";
     {
-      substituters = [ "http://hut/cache" ];
-      trusted-public-keys = [ "jungle.bsc.es:pEc7MlAT0HEwLQYPtpkPLwRsGf80ZI26aj29zMw/HH0=" ];
+      extra-substituters = [ "http://hut/cache" ];
+      extra-trusted-public-keys = [ "jungle.bsc.es:pEc7MlAT0HEwLQYPtpkPLwRsGf80ZI26aj29zMw/HH0=" ];
     };
 }
--- m/raccoon/configuration.nix
+++ m/raccoon/configuration.nix
@@ -29,8 +29,8 @@
   };
 
   nix.settings = {
-    substituters = [ "https://jungle.bsc.es/cache" ];
-    trusted-public-keys = [ "jungle.bsc.es:pEc7MlAT0HEwLQYPtpkPLwRsGf80ZI26aj29zMw/HH0=" ];
+    extra-substituters = [ "https://jungle.bsc.es/cache" ];
+    extra-trusted-public-keys = [ "jungle.bsc.es:pEc7MlAT0HEwLQYPtpkPLwRsGf80ZI26aj29zMw/HH0=" ];
   };
 
   # Enable performance governor
