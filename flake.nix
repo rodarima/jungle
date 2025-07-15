37c12783bb1bc969c7b2dc8364321bff35795848
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2025-07-15 17:45:40 +0200

    Upgrade nixpkgs to nixos 25.05
    Reviewed-by: Aleix Roca Nonell <aleix.rocanonell@bsc.es>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
     bscpkgs.url = "git+https://git.sr.ht/~rodarima/bscpkgs";
