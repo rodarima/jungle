142985c505f66d1820a357445cab3502d48059e3
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2025-07-23 13:42:57 +0200

    Move August shutdown to 3rd at 22h
    Reviewed-by: Aleix Roca Nonell <aleix.rocanonell@bsc.es>
    Reviewed-by: Aleix Boné <abonerib@bsc.es>

--- m/common/base/august-shutdown.nix
+++ m/common/base/august-shutdown.nix
@@ -1,12 +1,12 @@
 {
-  # Shutdown all machines on August 2nd at 11:00 AM, so we can protect the
+  # Shutdown all machines on August 3rd at 22:00, so we can protect the
   # hardware from spurious electrical peaks on the yearly electrical cut for
   # manteinance that starts on August 4th.
   systemd.timers.august-shutdown = {
-    description = "Shutdown on August 2nd for maintenance";
+    description = "Shutdown on August 3rd for maintenance";
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar = "*-08-02 11:00:00";
+      OnCalendar = "*-08-03 22:00:00";
       RandomizedDelaySec = "10min";
       Unit = "systemd-poweroff.service";
     };
