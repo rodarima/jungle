74130214403c7ec78317584a0c5018e8fae07a66
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2025-08-27 12:59:21 +0200

    Add firewall rules to slurm server
    Reviewed-by: Aleix Boné <abonerib@bsc.es>

--- m/module/slurm-server.nix
+++ m/module/slurm-server.nix
@@ -6,4 +6,13 @@
   ];
 
   services.slurm.server.enable = true;
+
+  networking.firewall = {
+    extraCommands = ''
+      # Accept slurm connections to controller from compute nodes
+      iptables -A nixos-fw -p tcp -s 10.0.40.0/24 --dport 6817 -j nixos-fw-accept
+      # Accept slurm connections from compute nodes for srun
+      iptables -A nixos-fw -p tcp -s 10.0.40.0/24 --dport 60000:61000 -j nixos-fw-accept
+    '';
+  };
 }
