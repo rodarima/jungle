dff6eaf5876d4d6a97fe4b78e3afc25f15673a58
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2025-08-29 14:46:24 +0200

    Accept fox connection to slurm controller
    Reviewed-by: Aleix Roca Nonell <aleix.rocanonell@bsc.es>

--- m/module/slurm-server.nix
+++ m/module/slurm-server.nix
@@ -13,6 +13,11 @@
       iptables -A nixos-fw -p tcp -s 10.0.40.0/24 --dport 6817 -j nixos-fw-accept
       # Accept slurm connections from compute nodes for srun
       iptables -A nixos-fw -p tcp -s 10.0.40.0/24 --dport 60000:61000 -j nixos-fw-accept
+
+      # Accept slurm connections to controller from fox (via wireguard)
+      iptables -A nixos-fw -p tcp -i wg0 -s 10.100.0.1/32 --dport 6817 -j nixos-fw-accept
+      # Accept slurm connections from fox for srun (via wireguard)
+      iptables -A nixos-fw -p tcp -i wg0 -s 10.100.0.1/32 --dport 60000:61000 -j nixos-fw-accept
     '';
   };
 }
